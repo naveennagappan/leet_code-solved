7fd6340f2048ca3199ac903bdbf923ebc51e320d
naveennagappan <naveensona45@gmail.com>
2026-09-21 06:28:13 +0530

    Time: 0 ms (100%), Space: 43.4 MB (78.84%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,10 +3,10 @@ class Solution {
         int k=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=val){
-                    nums[k]=nums[i];
-                    k++;
+                nums[k]=nums[i];
+                k++;
             }
         }
-        return k;
+        return k; 
     }
 }
