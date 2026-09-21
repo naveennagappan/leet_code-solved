28a70e94d7b0ba6054be1ae1384079dcb55e02fa
naveennagappan <naveensona45@gmail.com>
2026-09-21 06:17:08 +0530

    Time: 2 ms (99.39%), Space: 47.2 MB (24.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map=new HashMap<>();
+        HashMap<Integer,Integer>  map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             int compliment=target-nums[i];
             if(map.containsKey(compliment)){
@@ -8,6 +8,6 @@ class Solution {
             }
             map.put(nums[i],i);
         }
-        return new int[]{};
+        return new int[]{-1,-1};
     }
 }
